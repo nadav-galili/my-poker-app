d3dee4cf0e533e0cdc21c5b791c103e0c46af09d
nadav-galili <67638018+nadav-galili@users.noreply.github.com>
2021-03-15 15:11:03 +0200

    fixed lifting state up & added player table comp

--- src/components/games.jsx
+++ src/components/games.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
-// import Player from "./player";
+import GameTable from "./gameTable";
 import Players from "./players";
-// import DayPicker from "./common/date-picker";
 import http from "../services/httpService";
 import { apiUrl } from "../config/config.json";
 // import axios from "axios";
 import playersService from "../services/playersService";
 import gameService from "../services/gameService ";
-import { LinearProgress } from "@material-ui/core";
+import PlayerTable from "./playersTable";
 
 class Games extends Component {
   state = {
@@ -29,39 +28,27 @@ class Games extends Component {
   addCashing = (playerId) => {
     let { players } = this.state;
     players[playerId - 1].cashing += 50;
+
     this.setState({ players });
   };
 
-  onInputChange = (event, playerId) => {
-    console.log(event.target.value, playerId);
+  onInputChange = (e) => {
     let { players } = this.state;
-    players[playerId - 1].cashInHand = event.target.value;
-    console.log(players);
+    players[e.id - 1].cashInHand = e.cashInHand;
     this.setState({ players });
   };
 
   updateGame = () => {
     let players = this.state.players;
-    players = players.filter((player) => player.selected);
 
-    // players.sort(function (a, b) {
-    //   return a.profit - b.profit;
-    // });
-
-    // let game_rank = 1;
-    // for (let i = 0; i < players.length; i++) {
-    //   if (i > 0 && players[i].profit < players[i - 1].profit) {
-    //     game_rank++;
-    //   }
-    //   players[i].game_rank = game_rank;
-    // }
+    // players = players.filter((player) => player.selected);
 
     players.map((player) => {
       player.profit = player.cashInHand - player.cashing;
       player.num_of_cashing = player.cashing / 50;
 
       this.setState({ players });
-
+      console.log(this.state);
       return gameService.postGames(player);
     });
   };
@@ -82,67 +69,25 @@ class Games extends Component {
             />
           ))}
         </div>
+        <table className="table">
+          <GameTable />
+          <tbody>
+            {players.map((player) => (
+              <PlayerTable
+                key={player.id}
+                player={player}
+                addCashing={() => {
+                  this.addCashing(player.id);
+                }}
+                changeInput={this.onInputChange}
+                getId={this.getId}
+              />
+            ))}
+          </tbody>
+        </table>
         <button type="button" onClick={this.updateGame}>
           update
         </button>
-        <table className="table">
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>name</td>
-              <td></td>
-              <td>Current cash</td>
-              <td>Cash in hand</td>
-              <td>Final profit/loss</td>
-            </tr>
-          </thead>
-          <tbody>
-            {players.map((player) => (
-              <tr key={player.id}>
-                <td>{player.selected && player.id}</td>
-                <td>{player.selected && player.name}</td>
-                <td>
-                  {player.selected && (
-                    <button
-                      onClick={() => this.addCashing(player.id)}
-                      className="btn btn-secondary"
-                    >
-                      add 50
-                    </button>
-                  )}
-                </td>
-                <td>{player.selected && player.cashing}</td>
-                <td>
-                  <input
-                    type="number"
-                    onChange={(e) => this.onInputChange(e, player.id)}
-                  />
-                </td>
-
-                <td>
-                  <b>
-                    <i>{player.cashInHand - player.cashing}</i>
-                  </b>
-                </td>
-              </tr>
-            ))}
-            <tr>
-              <td>
-                <b>Total</b>
-              </td>
-              <td></td>
-              <td></td>
-
-              <td>
-                <b>
-                  {players.reduce((prev, cur) => {
-                    return prev + cur.cashing;
-                  }, 0)}
-                </b>
-              </td>
-            </tr>
-          </tbody>
-        </table>
       </div>
     );
   }
---
+++ src/components/playersTable.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class PlayerTable extends Component {
+  state = {};
+  handleChange = (e) => {
+    this.props.player.cashInHand = e.target.value;
+    this.props.changeInput(this.props.player);
+  };
+
+  render() {
+    const { player, addCashing } = this.props;
+
+    return (
+      <tr key={player.id}>
+        <td>{player.selected && player.id}</td>
+        <td>{player.selected && player.name}</td>
+
+        <td>
+          {player.selected && (
+            <button type="button" onClick={() => addCashing(player.id)}>
+              add cashing
+            </button>
+          )}
+        </td>
+        <td>{player.selected && player.cashing}</td>
+        <td>
+          <input type="number" onChange={this.handleChange} />
+        </td>
+        <td>
+          <i>
+            <b>{player.cashInHand - player.cashing}</b>
+          </i>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default PlayerTable;
+// const PlayerTable = ({ player, addCashing, onInputChange }) => {
+//   return (
+//     <tr key={player.id}>
+//       <td>{player.id}</td>
+//       <td>{player.name}</td>
+
+//       <td>
+//         <button type="button" onClick={() => addCashing(player.id)}>
+//           add cashing
+//         </button>
+//       </td>
+//       <td>{player.cashing}</td>
+//       <td>
+//         <input type="number" onChange={(e) => e} />
+//       </td>
+//     </tr>
+//   );
+// };
+
+// export default PlayerTable;
